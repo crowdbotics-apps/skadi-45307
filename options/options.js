7b0902c795ce0ab83427d802e3b29c50bcfca744
Crowdbotics <team@crowdbotics.com>
2024-01-07 06:54:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skadi-45307",
+  url: "https://skadi-45307.botics.co",
+  api: "https://skadi-45307.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
